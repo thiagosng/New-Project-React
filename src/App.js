e38177a2d8a37a0da795466ad5ff5227c042c2f9
thiagosng <thiagonunes026@gmail.com>
2021-06-07 16:45:06 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const globalState = {
+  title: 'Titulo aleatorio DSASD ',
+  body: 'Corpo do texto ',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com ', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter ');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  console.log('Nenhuma action encontrada');
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-BR'),
+          })
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
+      <button onClick={() => dispatch({ type: 'QUALQUER COISAS' })}>
+        SEM AÇÃO
+      </button>
+    </div>
   );
 }
 
--- src/contexts/AppContext/data.js
+++
@@ -1,5 +0,0 @@
-export const globalState = {
-  title: 'Titulo aleatorio DSASD',
-  body: 'Corpo do texto',
-  counter: 0,
-};
