98d69d89179bcfbda0c6bc1cea41d687fb669f6a
thiagosng <thiagonunes026@gmail.com>
2021-06-07 10:18:48 -0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,30 +1,76 @@
 import P from 'prop-types';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
-import React, { useState, useEffect, useCallback } from 'react';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  console.log('filho, renderizou');
-  return <button onClick={() => incrementButton(10)}>+</button>;
-});
+const Post = ({ post, handleClick }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
-Button.propTypes = {
-  incrementButton: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+  handleClick: P.func,
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
+  console.log('Pai renderizou!');
 
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
+  // Component did mount
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
 
-  console.log('Pai, renderizou');
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
 
+  useEffect(() => {
+    contador.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
   return (
     <div className="App">
-      <p>Teste 3</p>
-      <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      <h6>Renderizou: {contador.current}x</h6>
+      <p>
+        <input
+          ref={input}
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} handleClick={handleClick} />;
+          })
+        );
+      }, [posts])}
+
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
